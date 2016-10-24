aa78da23e167801508996e893c85476cdcd336da
Mattia A. Fritz <mattia.fritz@unige.ch>
2016-10-24 15:49:19 +0200

    Ex and Tk 1

---
+++ examples/ex01-include-external/assets/js/my-js-file.js
@@ -0,0 +1,4 @@
+//This code will output a message to the page that includes it (don't worry if you don't understand it yet)
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.body.innerHTML += "<p><strong>This message is generated from the linked JavaScript file</strong></p>";
+});
---
+++ tasks/tk01-include-external/assets/js/app.js
@@ -0,0 +1,4 @@
+//This code will output a message to the page that includes it (don't worry if you don't understand it yet)
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.body.innerHTML += "<p><strong>3. Third file from your assets/js folder included!</strong></p>";
+});
